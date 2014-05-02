5a7617ff77bdc701cfb4993a0d36cb9d69036069
Josh Bassett <josh.bassett@gmail.com>
2014-05-02 10:59:34 +1000

    Calculate contiguous hexagon perimeters using Clipper.

--- assets/hexagon.js
+++ assets/hexagon.js
@@ -1,25 +1,33 @@
+var _ = require('lodash');
 var core = require('./core')
 
-function calculateVertices(position, radius) {
+function calculatePoints(position, radius) {
   var r = radius * Math.cos(core.degreesToRadians(30));
   var h = radius * Math.sin(core.degreesToRadians(30));
 
   return [
+    [position.x,           position.y + h               ],
     [position.x + r,       position.y                   ],
     [position.x + (2 * r), position.y + h               ],
     [position.x + (2 * r), position.y + h + radius      ],
     [position.x + r,       position.y + (2 * h) + radius],
-    [position.x,           position.y + h + radius      ],
-    [position.x,           position.y + h               ]
+    [position.x,           position.y + h + radius      ]
   ];
 }
 
 var Hexagon = function(position, radius) {
-  this.vertices = calculateVertices(position, radius);
+  this.vertices = calculatePoints(position, radius);
 };
 
 Hexagon.prototype = {
-  selected: false
+  selected: false,
+
+  // Returns true if the polygon contains a given vertex, false otherwise.
+  containsVertex: function(v) {
+    return _.some(this.vertices, function(w) {
+      return _.isEqual(v, w);
+    });
+  }
 };
 
 module.exports = Hexagon;
--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,38 @@
-// var allong = require('./allong.es').allong.es;
+var _ = require('lodash');
+var clipper = require('./clipper');
 var d3 = require('d3');
 var Hexgrid = require('./hexgrid')
 
-var RADIUS = 32,
-    ROWS = 10,
-    COLS = 10,
-    PADDING = 0;
+var RADIUS = 16,
+    ROWS = 20,
+    COLS = 20,
+    PADDING = 0,
+    SCALE = 100;
 
 var hexgrid = new Hexgrid(COLS, ROWS, RADIUS, PADDING);
+
+var hexagons = _.sample(hexgrid.hexagons, ROWS * COLS / 2);
+
+var cpr = new clipper.Clipper();
+    solutionPaths = [];
+
+var subjectPaths = _.map(hexagons, function(hexagon) {
+  var path = _.map(hexagon.vertices, function(vertex) {
+    return {X: vertex[0], Y: vertex[1]};
+  });
+  clipper.JS.ScaleUpPath(path, SCALE);
+  return path;
+});
+
+cpr.AddPaths(subjectPaths, clipper.PolyType.ptSubject, true);
+var result = cpr.Execute(clipper.ClipType.ctUnion, solutionPaths, clipper.PolyFillType.pftNonZero, clipper.PolyFillType.pftNonZero);
+
+var cells = _.map(solutionPaths, function(path) {
+  clipper.JS.ScaleDownPath(path, SCALE);
+  return _.map(path, function(point) {
+    return [point.X, point.Y];
+  });
+});
 
 var svg = d3
   .select('.container')
@@ -15,13 +40,21 @@ var svg = d3
   .attr('width', 640)
   .attr('height', 480);
 
-var polygon = svg
+svg
   .append('g')
   .selectAll('polygon')
-  .data(hexgrid.hexagons)
-  .enter()
-  .append('svg:polygon')
+  .data(hexagons)
+  .enter().append('svg:polygon')
+  .attr('class', 'hexagon')
   .attr('points', function(d, i) { return d.vertices.join(' '); })
   .on('mousedown', function(hexagon) {
     d3.select(this).classed('selected', true)
   });
+
+svg
+  .append('g')
+  .selectAll('polygon')
+  .data(cells)
+  .enter().append('svg:polygon')
+  .attr('class', 'cell')
+  .attr('points', function(d, i) { return d.join(' '); });
