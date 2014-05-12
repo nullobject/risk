66f35c179f7c296d9d3cabe408e6bd933e2b7056
Josh Bassett <josh.bassett@gmail.com>
2014-05-12 19:01:02 +1000

    Switch to npm and browserify.

---
+++ test/polygon_test.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+
+var Polygon = require('../src/polygon');
+
+describe('Polygon', function() {
+  describe('#containsPoint', function() {
+    p = new Polygon([[-1, -1], [-1, 1], [1, 1], [1, -1]]);
+
+    it('should return true if the polygon contains a given point', function() {
+      expect(p.containsPoint([0, 0])).to.be.true;
+    });
+
+    it('should return false if the polygon does not contain a given point', function() {
+      expect(p.containsPoint([2, 2])).to.be.false;
+    });
+  });
+});
