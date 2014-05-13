6e167bd53c9f2d7a986c006f564057718cf9757a
Josh Bassett <josh.bassett@gmail.com>
2014-05-13 21:03:55 +1000

    Cleanups.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ var d3    = require('d3');
 var Bacon = require('baconjs').Bacon;
 var World = require('./world')
 
-// SVG dimensions.
 var width = 640, height = 480;
-
 var world = new World(width, height);
+var selections = new Bacon.Bus();
 
 var svg = d3
   .select('.container')
@@ -14,26 +13,35 @@ var svg = d3
   .attr('width', width)
   .attr('height', height);
 
-// Draw hexagons.
-svg
-  .append('g')
-  .selectAll('polygon')
-  .data(world.hexagons)
-  .enter().append('svg:polygon')
-  .attr('class', 'hexagon')
-  .attr('points', function(d, i) { return d.join(' '); });
-
-// Draw countries.
-var selections = new Bacon.Bus();
-
+var hexgrid = svg.append('g');
 var countries = svg.append('g');
+var regions = svg.append('g');
+
+drawHexgrid();
+drawCountries(null);
+drawRegions();
+
+// TODO: If the country is a nearby country of the currently selected country,
+// then trigger a 'move' event. Otherwise, trigger a 'select' event.
+selections.scan(null, function(a, b) {
+  // Toggle the selection if the same country is selected.
+  return a === b ? null : b;
+}).onValue(drawCountries);
+
+function drawHexgrid() {
+  hexgrid
+    .selectAll('polygon')
+    .data(world.hexagons)
+    .enter().append('polygon')
+    .attr('class', 'hexagon')
+    .attr('points', function(d, i) { return d.join(' '); });
+}
 
 function drawCountries(selectedCountry) {
   var country = countries
     .selectAll('polygon')
     .data(world.countries);
 
-  // country.enter().append('g');
   country.enter().append('polygon');
 
   country
@@ -41,39 +49,19 @@ function drawCountries(selectedCountry) {
     .attr('class', function(country, i) { return 'cell q' + (i % 9) + '-9'; })
     .classed('selected', function(country) { return country === selectedCountry; })
     .classed('nearby', function(country) { return selectedCountry && _.contains(selectedCountry.neighbours, country); })
-    .on('mousedown', function(country) { selections.push(country); });
-
-  // country
-  //   .append('text')
-  //   .attr('transform', function(country) {
-  //     var centroid = calculateCentroid(this.previousSibling);
-  //     return 'translate(' + centroid[0] + ',' + centroid[1] + ')';
-  //   })
-  //   .text(function(country) { return country.armies; });
+    .on('click', function(country) { selections.push(country); });
 }
 
-selections.scan(null, function(a, b) {
-  // Toggle the selection if the same country is selected.
-  return a === b ? null : b;
-}).onValue(drawCountries);
+function drawRegions() {
+  regions
+    .selectAll('path')
+    .data(world.regions, polygon)
+    .enter().append('path')
+    .attr('class', 'voronoi')
+    .attr('d', polygon)
+    .order();
+}
 
-drawCountries(null);
-
-// function calculateCentroid(selection) {
-//   var bbox = selection.getBBox();
-//   return [bbox.x + bbox.width / 2, bbox.y + bbox.height / 2];
-// }
-
-// Draw regions.
-// svg
-//   .append('g')
-//   .selectAll('path')
-//   .data(world.regions, polygon)
-//   .enter().append('path')
-//   .attr('class', 'voronoi')
-//   .attr('d', polygon)
-//   .order();
-
-// function polygon(d) {
-//   return 'M' + d.join('L') + 'Z';
-// }
+function polygon(d) {
+  return 'M' + d.join('L') + 'Z';
+}
