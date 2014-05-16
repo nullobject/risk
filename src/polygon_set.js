a22698f6207b7d139935c0b157733785ae453ad1
Josh Bassett <josh.bassett@gmail.com>
2014-05-16 18:49:00 +1000

    Change PolygonSet to not inherit from array.

--- src/polygon_set.js
+++ src/polygon_set.js
@@ -3,18 +3,17 @@ var clipper = require('../lib/clipper');
 var SCALE = 100;
 
 var PolygonSet = function(polygons) {
-  polygons.__proto__ = PolygonSet.prototype;
-  return polygons;
+  this.polygons = polygons;
 };
 
-PolygonSet.prototype = [];
+PolygonSet.prototype.constructor = PolygonSet;
 
 // Merges the polygons in the polygon set.
 PolygonSet.prototype.merge = function() {
   var cpr = new clipper.Clipper(),
       solutionPaths = [];
 
-  var subjectPaths = this.map(function(polygon) {
+  var subjectPaths = this.polygons.map(function(polygon) {
     var path = polygon.map(function(vertex) {
       return {X: vertex[0], Y: vertex[1]};
     });
