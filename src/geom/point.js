7843954e12e2c423fe56f1776789991216228777
Josh Bassett <josh.bassett@gmail.com>
2014-10-18 22:30:33 +1100

    Add #distance function to point.

--- src/geom/point.js
+++ src/geom/point.js
@@ -4,15 +4,15 @@
 var Point = function() {
   var x, y, a = arguments;
 
-  if (a.length == 1) {
-    if (a[0] instanceof Array) {
+  if (a.length === 1) {
+    if (Array.isArray(a[0])) {
       x = a[0][0];
       y = a[0][1];
     } else {
       x = a[0].x;
       y = a[0].y;
     }
-  } else if (a.length == 2) {
+  } else if (a.length === 2) {
     x = a[0];
     y = a[1];
   }
@@ -27,6 +27,13 @@ var Point = function() {
 
     divide: function(n) {
       return Point(this.x / n, this.y / n);
+    },
+
+    distance: function(p) {
+      var dx = this.x - p.x,
+          dy = this.y - p.y;
+
+      return Math.sqrt((dx * dx) + (dy * dy));
     },
 
     toString: function() {
