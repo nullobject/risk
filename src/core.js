5e9c6826a485a0c8d8dba9b2fc68862a8e355157
Josh Bassett <josh.bassett@gmail.com>
2014-10-27 13:51:43 +1100

    Add .distribute function to core.

--- src/core.js
+++ src/core.js
@@ -20,4 +20,42 @@ module.exports = {
       set.subtract(as).union(bs);
     });
   }),
+
+  // Distributes `n` units round-robin using the availability list of `as`.
+  distribute: function(n, as) {
+    var bs = F.replicate(as.length, 0);
+
+    return distributeInto(n, as, bs);
+
+    // Recursively distributes `n` units into the list of `bs` using the
+    // availability list of `as`.
+    function distributeInto(n, as, bs) {
+      if (n <= 0 || F.sum(bs) >= F.sum(as)) {
+        return bs;
+      } else {
+        // Calculate the difference list.
+        var cs = F.zip(bs, as).map(F.uncurry(F.sub));
+
+        // Calculate the number of units to distribute in the next pass.
+        var m = Math.min(n, cs.filter(F.id).length);
+
+        return distributeInto(n - m, as, pass(m, bs, cs));
+      }
+    }
+
+    // Performs a pass over the list of `bs`, distributing `m` units using the
+    // difference list of `cs`.
+    function pass(m, bs, cs) {
+      var ds = bs.concat();
+
+      for (var i = 0; i < cs.length && m > 0; i++) {
+        if (cs[i] > 0) {
+          m -= 1;
+          ds[i] += 1;
+        }
+      }
+
+      return ds;
+    }
+  },
 };
--- test/core_test.js
+++ test/core_test.js
@@ -16,4 +16,17 @@ describe('core', function() {
       expect(core.replace([s, t], [u, v], set).toArray()).to.eql([u, v]);
     });
   });
+
+  describe('.distribute', function() {
+    it('should distribute the units', function() {
+      expect(core.distribute(0, [1, 2, 3])).to.eql([0, 0, 0]);
+      expect(core.distribute(1, [1, 2, 3])).to.eql([1, 0, 0]);
+      expect(core.distribute(2, [1, 2, 3])).to.eql([1, 1, 0]);
+      expect(core.distribute(3, [1, 2, 3])).to.eql([1, 1, 1]);
+      expect(core.distribute(4, [1, 2, 3])).to.eql([1, 2, 1]);
+      expect(core.distribute(5, [1, 2, 3])).to.eql([1, 2, 2]);
+      expect(core.distribute(6, [1, 2, 3])).to.eql([1, 2, 3]);
+      expect(core.distribute(7, [1, 2, 3])).to.eql([1, 2, 3]);
+    });
+  });
 });
