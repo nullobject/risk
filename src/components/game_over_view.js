090a5e16103ae4f4b42ce78dced63004c598a94f
Josh Bassett <josh.bassett@gmail.com>
2018-01-25 13:32:24 +1100

    Add game over modal

---
+++ src/components/game_over_view.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import classnames from 'classnames'
+import styles from '../stylesheets/styles.scss'
+
+export default class GameOverView extends React.PureComponent {
+  render () {
+    const bus = this.props.bus
+
+    return (
+      <div className={classnames(styles.modal, styles.row)}>
+        <div className={classnames(styles.container, styles.center, styles['align-self-center'])}>
+          <h1>Game Over</h1>
+          <button onClick={() => bus.emit('restart')}>Restart</button>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/root_component.js
+++ src/components/root_component.js
@@ -1,10 +1,10 @@
 import ControlsComponent from './controls_component'
 import GameComponent from './game_component'
+import GameOverView from './game_over_view'
 import HexgridComponent from './hexgrid_component'
 import PlayersComponent from './players_component'
 import React from 'react'
 import log from '../log'
-import styles from '../stylesheets/styles.scss'
 
 export default class RootComponent extends React.PureComponent {
   render () {
@@ -14,24 +14,15 @@ export default class RootComponent extends React.PureComponent {
     log.debug('RootComponent#render')
 
     return (
-      <div className={styles.game}>
+      <React.Fragment>
         <PlayersComponent currentPlayer={game.currentPlayer} game={game} />
         <svg width={game.world.width} height={game.world.height}>
           <HexgridComponent width={game.world.width} height={game.world.height} hexgrid={game.world.hexgrid} />
           <GameComponent bus={bus} game={game} />
-          {this.renderGameOver(game)}
         </svg>
         <ControlsComponent currentPlayer={game.currentPlayer} bus={bus} />
-      </div>
+        {game.over ? <GameOverView bus={bus} /> : null}
+      </React.Fragment>
     )
-  }
-
-  renderGameOver (game) {
-    return game.over ? (
-      <g className={styles.gameOver}>
-        <rect width='100%' height='100%' />
-        <text x='50%' y='50%'>Game Over</text>
-      </g>
-    ) : null
   }
 }
