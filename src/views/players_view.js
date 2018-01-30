06c8bd8f9cfecbc51eff352d8d9306eed9bc8788
Josh Bassett <josh.bassett@gmail.com>
2018-01-30 21:25:05 +1100

    Disable dead players

--- src/views/players_view.js
+++ src/views/players_view.js
@@ -17,11 +17,13 @@ export default class PlayersView extends React.PureComponent {
 
   renderPlayer (game) {
     return (player, index) => {
+      const armies = game.armiesForPlayer(player)
       const selected = player === this.props.currentPlayer
-      const className = classnames(styles[player], {[styles.selected]: selected})
+      const disabled = armies === 0
+      const className = classnames(styles[player], {[styles.selected]: selected, [styles.disabled]: disabled})
 
       return (
-        <li className={className} key={index}><span>{game.armiesForPlayer(player)}</span></li>
+        <li className={className} key={index}><span>{armies}</span></li>
       )
     }
   }
