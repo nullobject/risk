05e3da949a2d894e76d8f141ae1cdc453f92323d
Josh Bassett <josh.bassett@gmail.com>
2018-01-31 17:41:19 +1100

    Simplify copyright

--- src/views/help_view.js
+++ src/views/help_view.js
@@ -8,6 +8,8 @@ export default class HelpView extends React.PureComponent {
     return (
       <div className={styles.modal}>
         <div className={styles.container}>
+          <h1>Risk</h1>
+
           <h2>How to Play</h2>
 
           <p>The game of Risk is played with two or more players on a map. The object of the
@@ -25,7 +27,7 @@ export default class HelpView extends React.PureComponent {
 
           <h2>Credits</h2>
 
-          <p>Copyright © 2018 Josh Bassett</p>
+          <p>© 2018 Josh Bassett</p>
 
           <p className={styles.center}><button onClick={() => bus.emit('pause')}>Resume</button></p>
         </div>
